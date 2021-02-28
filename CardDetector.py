e2f7e572ba2e83686775e6d8620f796d15fe82ba
liushiqi9 <liushiqi9@gmail.com>
2021-02-28 19:04:39 +0800

    support ggpoker cards.

--- CardDetector.py
+++ CardDetector.py
@@ -10,61 +10,53 @@
 import cv2
 import numpy as np
 import time
-import os
 import Cards
-import VideoStream
+import os
 
 
 ### ---- INITIALIZATION ---- ###
 # Define constants and initialize variables
 
-## Camera settings
+# Camera settings
 IM_WIDTH = 1280
-IM_HEIGHT = 720 
+IM_HEIGHT = 720
 FRAME_RATE = 10
 
-## Initialize calculated frame rate because it's calculated AFTER the first time it's displayed
+# Initialize calculated frame rate because it's calculated AFTER the first time it's displayed
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
-## Define font to use
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-# Initialize camera object and video feed from the camera. The video stream is set up
-# as a seperate thread that constantly grabs frames from the camera feed. 
-# See VideoStream.py for VideoStream class definition
-## IF USING USB CAMERA INSTEAD OF PICAMERA,
-## CHANGE THE THIRD ARGUMENT FROM 1 TO 2 IN THE FOLLOWING LINE:
-videostream = VideoStream.VideoStream((IM_WIDTH,IM_HEIGHT),FRAME_RATE,1,0).start()
-time.sleep(1) # Give the camera time to warm up
+# Define font to use
+# font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Load the train rank and suit images
 path = os.path.dirname(os.path.abspath(__file__))
-train_ranks = Cards.load_ranks( path + '/Card_Imgs/')
-train_suits = Cards.load_suits( path + '/Card_Imgs/')
+train_ranks = Cards.load_ranks(path + "/Card_Imgs/ggpoker/new_isolate/")
+train_suits = Cards.load_suits(path + "/Card_Imgs/ggpoker/new_isolate/")
 
 
 ### ---- MAIN LOOP ---- ###
 # The main loop repeatedly grabs frames from the video stream
 # and processes them to find and identify playing cards.
 
-cam_quit = 0 # Loop control variable
 
 # Begin capturing frames
-while cam_quit == 0:
 
-    # Grab frame from video stream
-    image = videostream.read()
+# Grab frame from video stream
+img_path = "/home/yjb/poker_ai/OpenCV-Playing-Card-Detector/OCR/handcard.jpeg"
+# x: 278:718, y: 315:425
+ori_image = cv2.imread(img_path)
 
-    # Start timer (for calculating frame rate)
-    t1 = cv2.getTickCount()
+# BoardCards
+def ParseBoardCard(ori_image):
+    image = ori_image[310:425, 270:718]
 
     # Pre-process camera image (gray, blur, and threshold it)
     pre_proc = Cards.preprocess_image(image)
-	
+    cv2.imshow("pre_pro", pre_proc)
     # Find and sort the contours of all cards in the image (query cards)
     cnts_sort, cnt_is_card = Cards.find_cards(pre_proc)
-
+    # print(cnts_sort)
     # If there are no contours, do nothing
     if len(cnts_sort) != 0:
 
@@ -75,50 +67,76 @@ while cam_quit == 0:
 
         # For each contour detected:
         for i in range(len(cnts_sort)):
-            if (cnt_is_card[i] == 1):
-
+            if cnt_is_card[i] == 1:
+                print("detect one card, try to analyze it.")
                 # Create a card object from the contour and append it to the list of cards.
                 # preprocess_card function takes the card contour and contour and
                 # determines the cards properties (corner points, etc). It generates a
                 # flattened 200x300 image of the card, and isolates the card's
                 # suit and rank from the image.
-                cards.append(Cards.preprocess_card(cnts_sort[i],image))
+                cards.append(Cards.preprocess_card(cnts_sort[i], image, i))
 
                 # Find the best rank and suit match for the card.
-                cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff = Cards.match_card(cards[k],train_ranks,train_suits)
+                (
+                    cards[k].best_rank_match,
+                    cards[k].best_suit_match,
+                    cards[k].rank_diff,
+                    cards[k].suit_diff,
+                ) = Cards.match_card(cards[k], train_ranks, train_suits)
 
                 # Draw center point and match result on the image.
                 image = Cards.draw_results(image, cards[k])
                 k = k + 1
-	    
+
         # Draw card contours on image (have to do contours all at once or
         # they do not show up properly for some reason)
-        if (len(cards) != 0):
+        if len(cards) != 0:
             temp_cnts = []
             for i in range(len(cards)):
                 temp_cnts.append(cards[i].contour)
-            cv2.drawContours(image,temp_cnts, -1, (255,0,0), 2)
-        
-        
+            cv2.drawContours(image, temp_cnts, -1, (255, 0, 0), 2)
+
     # Draw framerate in the corner of the image. Framerate is calculated at the end of the main loop,
     # so the first time this runs, framerate will be shown as 0.
-    cv2.putText(image,"FPS: "+str(int(frame_rate_calc)),(10,26),font,0.7,(255,0,255),2,cv2.LINE_AA)
+    # cv2.putText(image, "FPS: "+str(int(frame_rate_calc)),
+    #            (10, 26), font, 0.7, (255, 0, 255), 2, cv2.LINE_AA)
 
     # Finally, display the image with the identified cards!
-    cv2.imshow("Card Detector",image)
+    # cv2.imshow("Card Detector", image)
+    cv2.imwrite("/home/yjb/img.png", image)
 
-    # Calculate framerate
-    t2 = cv2.getTickCount()
-    time1 = (t2-t1)/freq
-    frame_rate_calc = 1/time1
-    
-    # Poll the keyboard. If 'q' is pressed, exit the main loop.
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        cam_quit = 1
-        
+    key = cv2.waitKey(0) & 0xFF
+    if key == ord("c"):
+        cv2.destroyAllWindows()
+    # Close all windows and close the PiCamera video stream.
+    # cv2.destroyAllWindows()
 
-# Close all windows and close the PiCamera video stream.
-cv2.destroyAllWindows()
-videostream.stop()
+
+def ParseHandCard1(ori_image):
+    # ParseHandCard(ori_image, 420, 474, 564, 635)
+    ParseHandCard(ori_image, 564, 640, 420, 474)
+
+
+def ParseHandCard2(ori_image):
+    # ParseHandCard(ori_image, 420, 474, 564, 635)
+    ParseHandCard(ori_image, 561, 635, 475, 527)
+
+
+def ParseHandCard(ori_image, x1, x2, y1, y2):
+    Qcorner = ori_image[x1:x2, y1:y2]
+    # this image is already the corner of the card. we need to resize it and grey it.
+
+    card = Cards.preprocess_corner(Qcorner)
+    (best_rank_match, best_suit_match, rank_diff, suit_diff,) = Cards.match_card(
+        card, train_ranks, train_suits
+    )
+    print("Rank: ", best_rank_match, ",suit: ", best_suit_match)
+
+
+cv2.imshow("ori_image", ori_image)
+ParseHandCard1(ori_image)
+ParseHandCard2(ori_image)
+key = cv2.waitKey(0) & 0xFF
+if key == ord("c"):
+    cv2.destroyAllWindows()
 
--- Rank_Suit_Isolator.py
+++ Rank_Suit_Isolator.py
@@ -9,8 +9,11 @@ import time
 import Cards
 import os
 
-img_path = os.path.dirname(os.path.abspath(__file__)) + '/Card_Imgs/'
+img_path = (
+    os.path.dirname(os.path.abspath(__file__)) + "/Card_Imgs/ggpoker/new_isolate/"
+)
 
+print(img_path)
 IM_WIDTH = 1280
 IM_HEIGHT = 720
 
@@ -20,125 +23,132 @@ RANK_HEIGHT = 125
 SUIT_WIDTH = 70
 SUIT_HEIGHT = 100
 
-# If using a USB Camera instead of a PiCamera, change PiOrUSB to 2
-PiOrUSB = 1
-
-if PiOrUSB == 1:
-    # Import packages from picamera library
-    from picamera.array import PiRGBArray
-    from picamera import PiCamera
-
-    # Initialize PiCamera and grab reference to the raw capture
-    camera = PiCamera()
-    camera.resolution = (IM_WIDTH,IM_HEIGHT)
-    camera.framerate = 10
-    rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
-
-if PiOrUSB == 2:
-    # Initialize USB camera
-    cap = cv2.VideoCapture(0)
-
 # Use counter variable to switch from isolating Rank to isolating Suit
-i = 1
+i = 0
+image_dir = "/home/yjb/poker_ai/OpenCV-Playing-Card-Detector/Card_Imgs/ggpoker/"
+name_list = [
+    "Ace",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King",
+    "Spades",
+    "Diamonds",
+    "Clubs",
+    "Hearts",
+]
+for index in range(17):
+    i = i + 1
+    Name = name_list[i - 1]
+    source_file_name = "original_img/" + str(i) + ".png"
+    filename = Name + ".jpg"
 
-for Name in ['Ace','Two','Three','Four','Five','Six','Seven','Eight',
-             'Nine','Ten','Jack','Queen','King','Spades','Diamonds',
-             'Clubs','Hearts']:
-
-    filename = Name + '.jpg'
-
-    print('Press "p" to take a picture of ' + filename)
-    
-    
-
-    if PiOrUSB == 1: # PiCamera
-        rawCapture.truncate(0)
-        # Press 'p' to take a picture
-        for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-
-            image = frame.array
-            cv2.imshow("Card",image)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("p"):
-                break
-
-            rawCapture.truncate(0)
-
-    if PiOrUSB == 2: # USB camera
-        # Press 'p' to take a picture
-        while(True):
-
-            ret, frame = cap.read()
-            cv2.imshow("Card",frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("p"):
-                image = frame
-                break
-
+    print("start processing imgage: ", source_file_name)
+    image = cv2.imread(image_dir + source_file_name)
+    cv2.imshow(source_file_name, image)
     # Pre-process image
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    retval, thresh = cv2.threshold(blur,100,255,cv2.THRESH_BINARY)
-
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    retval, thresh = cv2.threshold(blur, 190, 255, cv2.THRESH_BINARY)
+    cv2.imshow("thresh", thresh)
     # Find contours and sort them by size
-    dummy,cnts,hier = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key=cv2.contourArea,reverse=True)
+    cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
     # Assume largest contour is the card. If there are no contours, print an error
     flag = 0
     image2 = image.copy()
 
     if len(cnts) == 0:
-        print('No contours found!')
+        print("No contours found!")
         quit()
 
     card = cnts[0]
 
     # Approximate the corner points of the card
-    peri = cv2.arcLength(card,True)
-    approx = cv2.approxPolyDP(card,0.01*peri,True)
+    peri = cv2.arcLength(card, True)
+    approx = cv2.approxPolyDP(card, 0.01 * peri, True)
     pts = np.float32(approx)
 
-    x,y,w,h = cv2.boundingRect(card)
+    x, y, w, h = cv2.boundingRect(card)
 
     # Flatten the card and convert it to 200x300
-    warp = Cards.flattener(image,pts,w,h)
+    print("pts: ", pts)
+    print("w: ", w)
+    print("h: ", h)
+    warp = Cards.flattener(image, pts, w, h)
+
+    x = 0
+    y = 0
+    w = 115
+    h = 85
+    w2 = 75
+    color = (255, 255, 0)
+    cv2.rectangle(warp, (x, y), (x + h, y + w), color, thickness=1)
+    cv2.rectangle(warp, (x, y + w), (x + h, y + w + w2), color, thickness=1)
 
     # Grab corner of card image, zoom, and threshold
-    corner = warp[0:84, 0:32]
-    #corner_gray = cv2.cvtColor(corner,cv2.COLOR_BGR2GRAY)
-    corner_zoom = cv2.resize(corner, (0,0), fx=4, fy=4)
-    corner_blur = cv2.GaussianBlur(corner_zoom,(5,5),0)
-    retval, corner_thresh = cv2.threshold(corner_blur, 155, 255, cv2. THRESH_BINARY_INV)
+    cv2.imshow("Warp" + str(i), warp)
+
+    rank_corner = warp[0:115, 0:85]
+    # key = cv2.waitKey(0) & 0xFF
+    rank_corner_zoom = cv2.resize(rank_corner, (0, 0), fx=4, fy=4)
+    rank_corner_blur = cv2.GaussianBlur(rank_corner_zoom, (5, 5), 0)
+    retval, rank_corner_thresh = cv2.threshold(
+        rank_corner_blur, 155, 255, cv2.THRESH_BINARY_INV
+    )
+    # cv2.imshow("Rank_corner" + str(i), rank_corner)
+    # cv2.imshow("Rank_cornerZoom" + str(i), rank_corner_zoom)
+    # cv2.imshow("Rank_cornerThresh" + str(i), rank_corner_thresh)
+
+    suit_corner = warp[115:190, 0:85]
+    # key = cv2.waitKey(0) & 0xFF
+    suit_corner_zoom = cv2.resize(suit_corner, (0, 0), fx=4, fy=4)
+    suit_corner_blur = cv2.GaussianBlur(suit_corner_zoom, (5, 5), 0)
+    retval, suit_corner_thresh = cv2.threshold(
+        suit_corner_blur, 155, 255, cv2.THRESH_BINARY_INV
+    )
+    # cv2.imshow("Suit_corner" + str(i), suit_corner)
+    # cv2.imshow("Suit_cornerZoom" + str(i), suit_corner_zoom)
+    # cv2.imshow("Suit_cornerThresh" + str(i), suit_corner_thresh)
+    # continue
 
     # Isolate suit or rank
-    if i <= 13: # Isolate rank
-        rank = corner_thresh[20:185, 0:128] # Grabs portion of image that shows rank
-        dummy, rank_cnts, hier = cv2.findContours(rank, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        rank_cnts = sorted(rank_cnts, key=cv2.contourArea,reverse=True)
-        x,y,w,h = cv2.boundingRect(rank_cnts[0])
-        rank_roi = rank[y:y+h, x:x+w]
+    if i <= 13:  # Isolate rank
+        # rank = corner_thresh[20:185, 0:128] # Grabs portion of image that shows rank
+        rank = rank_corner_thresh
+        rank_cnts, hier = cv2.findContours(rank, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        rank_cnts = sorted(rank_cnts, key=cv2.contourArea, reverse=True)
+        x, y, w, h = cv2.boundingRect(rank_cnts[0])
+        rank_roi = rank[y : y + h, x : x + w]
         rank_sized = cv2.resize(rank_roi, (RANK_WIDTH, RANK_HEIGHT), 0, 0)
         final_img = rank_sized
 
-    if i > 13: # Isolate suit
-        suit = corner_thresh[186:336, 0:128] # Grabs portion of image that shows suit
-        dummy, suit_cnts, hier = cv2.findContours(suit, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        suit_cnts = sorted(suit_cnts, key=cv2.contourArea,reverse=True)
-        x,y,w,h = cv2.boundingRect(suit_cnts[0])
-        suit_roi = suit[y:y+h, x:x+w]
+    if i > 13:  # Isolate suit
+        suit = suit_corner_thresh
+        cv2.imshow("Suit" + str(i), suit)
+        suit_cnts, hier = cv2.findContours(suit, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        suit_cnts = sorted(suit_cnts, key=cv2.contourArea, reverse=True)
+        x, y, w, h = cv2.boundingRect(suit_cnts[0])
+        suit_roi = suit[y : y + h, x : x + w]
         suit_sized = cv2.resize(suit_roi, (SUIT_WIDTH, SUIT_HEIGHT), 0, 0)
         final_img = suit_sized
 
-    cv2.imshow("Image",final_img)
+    cv2.imshow("Image", final_img)
+    cv2.imwrite(img_path + filename, final_img)
+    print("write into dir: ", img_path + filename)
 
-    # Save image
-    print('Press "c" to continue.')
-    key = cv2.waitKey(0) & 0xFF
-    if key == ord('c'):
-        cv2.imwrite(img_path+filename,final_img)
 
-    i = i + 1
-
-cv2.destroyAllWindows()
-camera.close()
+print('Press "c" to continue.')
+key = cv2.waitKey(0) & 0xFF
+if key == ord("c"):
+    cv2.destroyAllWindows()
+# camera.close()
