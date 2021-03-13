3b9d7b64780130bd7ba7bcc60f86df6c5d8ae3b6
liushiqi9 <liushiqi9@gmail.com>
2021-03-13 22:46:04 +0800

    add recognition server

---
+++ recognition_server_client.py
@@ -0,0 +1,51 @@
+import cv2
+import glob
+import base64
+import sys
+import os
+import numpy as np
+
+
+sys.path.append("/home/liushiqi9/workspace/slumbot2019/src/thrift/gen-py")
+sys.path.append(
+    "/home/liushiqi9/workspace/thrift-0.13.0/lib/py/build/lib.linux-x86_64-3.8/"
+)
+
+
+from recognition_server import RecognitionServer
+from recognition_server.ttypes import *
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+
+# Make socket
+transport = TSocket.TSocket("127.0.0.1", 9090)
+
+# Buffering is critical. Raw sockets are very slow
+transport = TTransport.TBufferedTransport(transport)
+
+# Wrap in a protocol
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+# Create a client to use the protocol encoder
+client = RecognitionServer.Client(protocol)
+
+# Connect!
+transport.open()
+
+img_path = "/home/liushiqi9/workspace/OpenCV-Playing-Card-Detector/OCR/3.png"
+ori_image = cv2.imread(img_path)
+img = base64.b64encode(ori_image)
+
+img_str = open(img_path, "rb").read()
+quotient = client.AnalyzeCard(bytearray(img_str))
+
+for hand_card in quotient.hole_cards:
+    print("HandCard: ", hand_card.rank, ", suit: ", hand_card.suit)
+for board_card in quotient.board_cards:
+    print("BoardCard: ", board_card.rank, ", suit: ", board_card.suit)
+
+transport.close()
---
+++ recognition_server_main.py
@@ -0,0 +1,97 @@
+import glob
+import sys
+import base64
+import cv2
+
+sys.path.append("/home/liushiqi9/workspace/slumbot2019/src/thrift/gen-py")
+sys.path.append(
+    "/home/liushiqi9/workspace/thrift-0.13.0/lib/py/build/lib.linux-x86_64-3.8/"
+)
+
+from recognition_server import RecognitionServer
+from recognition_server.ttypes import *
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.protocol import TJSONProtocol
+from thrift.server import TServer, THttpServer
+import http.server as BaseHTTPServer
+
+import CardDetector
+
+from PIL import Image
+import cv2
+from io import StringIO
+import numpy as np
+
+
+def readb64(base64_string):
+    sbuf = StringIO()
+    sbuf.write(base64.b64decode(base64_string))
+    pimg = Image.open(sbuf)
+    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+
+class RecognitionserverHandler:
+    def __init__(self):
+        self.log = {}
+
+    def AnalyzeCard(self, image):
+        """
+      Parameters:
+       - image
+
+      """
+        print("Recv analyze card")
+        # decoded_image = readb64(image)
+        nparr = np.frombuffer(image, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR in OpenCV 3.1
+        print("Recv data: ", base64.b64encode(image))
+        cv2.imshow("decoded_image", img_np)
+
+        (hand_card_1, hand_card_suit_1) = CardDetector.ParseHandCard1(img_np)
+        (hand_card_2, hand_card_suit_2) = CardDetector.ParseHandCard2(img_np)
+        board_cards = CardDetector.ParseBoardCard(img_np)
+
+        print("Hand: ", hand_card_1, ", hand suit: ", hand_card_suit_1)
+        print("Hand: ", hand_card_2, ", hand suit: ", hand_card_suit_2)
+        print("Board: ", board_cards)
+
+        result = RecognitionResult()
+        result.hole_cards = []
+        result.board_cards = []
+        result.hole_cards.append(Card(hand_card_1, hand_card_suit_1))
+        result.hole_cards.append(Card(hand_card_2, hand_card_suit_2))
+        for board_card in board_cards:
+            result.board_cards.append(
+                Card(board_card.best_rank_match, board_card.best_suit_match)
+            )
+
+        cv2.destroyAllWindows()
+        return result
+
+    def getStruct(self, key):
+        print("getStruct(%d)" % (key))
+        return self.log[key]
+
+
+if __name__ == "__main__":
+    handler = RecognitionserverHandler()
+    processor = RecognitionServer.Processor(handler)
+    transport = TSocket.TServerSocket(host="0.0.0.0", port=9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+
+    # You could do one of these for a multithreaded server
+    # server = TServer.TThreadedServer(
+    #     processor, transport, tfactory, pfactory)
+    # server = TServer.TThreadPoolServer(
+    #     processor, transport, tfactory, pfactory)
+
+    print("Starting the server...")
+    server.serve()
+    print("done.")
+
